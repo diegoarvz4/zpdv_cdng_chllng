dd389b38a028d072a230cb8678115712115c9e6a
diegoarvz4 <darvz.gtz@gmail.com>
2020-02-27 14:03:14 -0600

    Project Setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    questions: [
+      'How do you describe yourself as a developer?',
+      'Select the JavaScript based technologies',
+      'Introduce a Palindrome',
+      'Write a sentence in the left box. Write the same sentence but reversed in the right box',
+    ],
+    answers: {
+      'AngularJS': true,
+      'Ember': true,
+      'VueJS': true,
+      'Java': false,
+      'C#': false,
+    },
+    usersAnswers: [
+      '',
+      {
+        'AngularJS': false,
+        'Ember': false,
+        'VueJS': false,
+        'Java': false,
+        'C#': false,
+      },
+      '',
+      ['','']
+    ]
+  }
+
+
+  return() {
+    return (
+      <div className="App">
+
+      </div>
+    )
+  };
 }
 
 export default App;
